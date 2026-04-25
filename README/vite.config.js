4ffd4d0bb4723c2b173d26e6a705d59d2734f001
Aya Ayman <ayaaymna2004@gmail.com>
2026-04-26 02:35:48 +0300

    Add GitHub Actions deployment

--- README/vite.config.js
+++ README/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/3d-simuscience/', // مهم جدًا
-  base: "/"
+  base: '/3d-simuscience/', // اكتبي هنا اسم المشروع بالظبط زي ما هو في جيت هاب
 })
